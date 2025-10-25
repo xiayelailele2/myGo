b48bf98ef735114add03ca1cf6b03e72c4a0439c
wakaka <2321352131@qq.com>
2025-10-25 11:50:30 +0800

    Revert "master第四次commit"
    This reverts commit 282c3c68048f2df5a38c5dadfc2da617279faf80.

--- MasterSubmit.java
+++ MasterSubmit.java
@@ -2,6 +2,5 @@ public class MasterSubmit {
     public static void main(String[] args) {
         System.out.println("master的第二次添加");
         System.out.println("master的第三次添加");
-        System.out.println("master合并之前");
     }
 }
