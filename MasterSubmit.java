64807ef952f3212d47bae4e8f613c1bae11653dc
wakaka <2321352131@qq.com>
2025-10-25 12:51:53 +0800

    删除

--- MasterSubmit.java
+++ MasterSubmit.java
@@ -3,8 +3,8 @@ public class MasterSubmit {
         System.out.println("master的第二次添加");
         System.out.println("master的第三次添加");
         System.out.println("master合并之前");
-        System.out.println("master合并之后01");
-        System.out.println("master合并之后02");
+//        System.out.println("master合并之后01");
+//        System.out.println("master合并之后02");
     }
     public void say(){
         System.out.println("say something!");
